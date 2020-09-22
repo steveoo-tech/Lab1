fc951ab473f7822d75c384f286710bd1ed94bac4
steveoo-tech <60714891+steveoo-tech@users.noreply.github.com>
2020-09-22 00:42:58 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+//fahrenheit to celsius
+
+function fToC(ferenheit){
+
+    var input = ferenheit;
+    var fToCelcius = (input - 32) * 5/9;
+    var output = input + " ferenheit is " + fToCelcius + " celsius";
+    console.log(output);
+
+}
